08ed3001bb7b38e66177257e57c649359f78675e
avishka meddegoda <ameddegoda143@gmail.com>
2020-05-19 15:20:52 +0530

    Sorting and Filtering based on attributes

--- app/ApiResponser.php
+++ app/ApiResponser.php
@@ -20,6 +20,9 @@ trait ApiResponser{
             return $this->successResponse(['data' => $collection], $code);
         }
         $transformer = $collection->first()->transformer;
+
+        $collection = $this->filterData($collection, $transformer);
+        $collection = $this->sortData($collection, $transformer);
         $collection = $this->transformData($collection, $transformer);
 
         return $this->successResponse($collection, $code);
@@ -39,6 +42,26 @@ trait ApiResponser{
     protected function showMessage($message, $code = 200)
     {
         return $this->successResponse(['data' => $message], $code);
+    }
+
+    protected function filterData(Collection $collection, $transformer){
+        foreach(request()->query() as $query => $value){
+            $attribute = $transformer::originalAttribute($query);
+
+            if(isset($attribute, $value)){
+                $collection = $collection->where($attribute, $value);
+            }  
+        }
+        return $collection;
+    }
+
+    protected function sortData(Collection $collection, $transformer){
+        if(request()->has('sort_by')){
+            $attribute = $transformer::originalAttribute(request()->sort_by);
+
+            $collection = $collection->sortBy->{$attribute};
+        }
+        return $collection;
     }
 
     protected function transformData($data, $transformer){
